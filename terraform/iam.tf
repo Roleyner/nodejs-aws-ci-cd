892a646536a3e221ebb581d810d511fc107b4dd0
Roleyner Perdomo <roleyner@aidence.com>
2023-07-25 16:18:24 +0200

    first commit

---
+++ terraform/iam.tf
@@ -0,0 +1,99 @@
+resource "aws_iam_role" "publisher" {
+  name = "ecr-role"
+  path = "/serviceaccounts/"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codebuild.amazonaws.com" # GitHub Actions uses CodeBuild service for role assumption
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "fargate" {
+  name = "fargate-role"
+  path = "/serviceaccounts/"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = [
+            "ecs.amazonaws.com",
+            "ecs-tasks.amazonaws.com"
+          ]
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy" "publisher" {
+  name = "ecr-publisher-role"
+  role = aws_iam_role.publisher.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "iam:PassRole",
+        "iam:GetRole",
+        "ecs:DescribeTaskDefinition",
+        "ecs:DescribeServices",
+        "ecs:UpdateService",
+        "ecs:RegisterTaskDefinition",
+        "ecr:CompleteLayerUpload",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:GetAuthorizationToken",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetLifecyclePolicy",
+        "ecr:InitiateLayerUpload",
+        "ecr:PutImage",
+        "ecr:UploadLayerPart"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "fargate" {
+  name = "fargate-execution-role"
+  role = aws_iam_role.fargate.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecr:CompleteLayerUpload",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:GetAuthorizationToken",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetLifecyclePolicy"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+container = {
+  image = "particule/helloworld"
+}
+
+vpc = {
+  id = "vpc-02559f7e614b4fb26"
+}
